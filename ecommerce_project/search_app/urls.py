67a452a0ba1bf9511962a6088aef4a3b810e3b85
vjoxgit7code <vigneshr4180@gmail.com>
2024-01-31 16:59:16 +0530

    remove all

--- ecommerce_project/search_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'search_app'
-
-urlpatterns = [
-    path('', views.search_view, name='search_view'),  # Name it 'search_view'
-]
--- ecommerce_project/search_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from shop.models import Product
-from django.db.models import Q
-
-def search_view(request):
-    query = request.GET.get('q', '')
-    products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query, available=True))
-    return render(request, 'search.html', {'query': query, 'products': products})
-
